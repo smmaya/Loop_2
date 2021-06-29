7dc495bd4c985d21c76edd2b96e4831a2854ff6d
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 14:13:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int j, n;
+
+            Console.Write("\n\n");
+            Console.Write("Napisz tabliczkę mnożenia :\n");
+            Console.Write("\n\n");
+
+            Console.Write("Wpisz liczbę : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (j = 1; j <= 10; j++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, j, n * j);
+            }
+        }
+    }
+}
